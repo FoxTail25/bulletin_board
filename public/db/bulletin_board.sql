9f2e98916ca72c5a3a170d22888e27ea1e9d0150
Fox Tail <foxtail25@gmail.com>
2025-07-14 17:40:34 +0300

    дб

--- public/db/bulletin_board.sql
+++ public/db/bulletin_board.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.127.126.26
--- Время создания: Июл 14 2025 г., 09:35
+-- Время создания: Июл 14 2025 г., 17:40
 -- Версия сервера: 8.0.35
--- Версия PHP: 8.1.28
+-- Версия PHP: 8.2.18
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,13 +24,40 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Структура таблицы `advertisements`
+--
+
+CREATE TABLE `advertisements` (
+  `id` int NOT NULL,
+  `slug` varchar(32) COLLATE utf8mb4_general_ci NOT NULL,
+  `ad_head` text COLLATE utf8mb4_general_ci NOT NULL,
+  `ad_body` text COLLATE utf8mb4_general_ci NOT NULL,
+  `subject_id` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Дамп данных таблицы `advertisements`
+--
+
+INSERT INTO `advertisements` (`id`, `slug`, `ad_head`, `ad_body`, `subject_id`) VALUES
+(1, '1752492910', 'test head', 'test_body', 2),
+(2, '1752492988', 'test head2', 'test body2', 2),
+(3, '1752493063', 'goods test', 'goods test body', 1),
+(4, '1752494334', 'Заголовок3', 'Объявление 3', 2),
+(5, '1752494378', 'Заголовок4', 'Объявление 4', 2),
+(6, '1752494423', 'Товар 2', 'Продам товар 2 продам', 1),
+(7, '1752494443', 'товар 3', 'Куплю товар 3 товар', 1);
+
+-- --------------------------------------------------------
+
+--
 -- Структура таблицы `subject`
 --
 
 CREATE TABLE `subject` (
   `id` int NOT NULL,
-  `slug` varchar(16) COLLATE utf8mb4_general_ci NOT NULL,
-  `name` varchar(32) COLLATE utf8mb4_general_ci NOT NULL
+  `slug` varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `name` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -46,6 +73,12 @@ INSERT INTO `subject` (`id`, `slug`, `name`) VALUES
 --
 
 --
+-- Индексы таблицы `advertisements`
+--
+ALTER TABLE `advertisements`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Индексы таблицы `subject`
 --
 ALTER TABLE `subject`
@@ -54,6 +87,12 @@ ALTER TABLE `subject`
 --
 -- AUTO_INCREMENT для сохранённых таблиц
 --
+
+--
+-- AUTO_INCREMENT для таблицы `advertisements`
+--
+ALTER TABLE `advertisements`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT для таблицы `subject`
